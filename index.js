0b98476e5245ff6384937ae192538645e7076a8d
David Thomas <david.thomas1@digital.justice.gov.uk>
2019-06-10 14:00:45 +0100

    Create park run script

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const Mustache = require("mustache")
+const parkRunData = require('./data/parkRunData.json')
+const template = require('./htmlTemplate')
+const result = Mustache.render(template, parkRunData)
+
+//create a file named suoernigel.txt:
+fs.writeFile('supernigel.html', result , function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
